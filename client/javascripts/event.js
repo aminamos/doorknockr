f275a5593865c1fb132e25ee5f0f276040d3a3f7
aminamos <26092352+aminamos@users.noreply.github.com>
2020-01-15 10:59:23 -0800

    walkthrough testing

--- client/javascripts/event.js
+++ client/javascripts/event.js
@@ -1,11 +1,40 @@
+// class Event {
+//   constructor(title, date, description) {
+//     this.title = title
+//     this.date = date
+//     this.description = description
+//     this.fetchEvents
+//     this.render
+//   }
+
+//   get fetchEvents() {
+//     fetch('http://localhost:3000/events')
+//     .then(
+//       function(response) {
+//         return response.json()
+//       }
+//     ).then(function(json) {
+//       populatePageWithEvents(json)
+//     })
+//   }
+  
+//   populatePageWithEvents() {
+//     let eventContainer = document.querySelector('.all-events')
+//     console.log('hi')
+//   }
+
+//   get render() {
+//     let eventContainer = document.querySelector('.all-events')
+//     eventContainer.innerHTML += 'hi'
+//   }
+// }
+
 class Event {
   constructor(title, date, description) {
     this.title = title
     this.date = date
     this.description = description
-  }
-
-  render() {
-    
+    this.fetchEvents
+    this.render
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  root to: "home#index"
   resources :events
 end
 
